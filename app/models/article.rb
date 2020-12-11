2df9d4b56a21585350e4e307cf8648aaf8dc0506
stephanie rousset <strabon@club-internet.fr>
2020-12-11 19:30:53 +0100

    add a search bar

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,4 +5,11 @@ class Article < ApplicationRecord
 
   validates :title, presence: true
   validates :content, presence: true
+
+  include PgSearch::Model
+  pg_search_scope :search_by_title_and_content,
+    against: [ :title, :content ],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
 end
